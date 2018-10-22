4a0a2c217143c0d78148576f62361a3ce3de78ab
Chaelor <joshsblizzard@gmail.com>
2018-10-22 11:41:19 -0500

    Pushing template to server

---
+++ src/Pages/Saved/Saved.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Saved.css';
+
+class Saved extends Component {
+
+  render() {
+    return{
+
+    }
+  }
+}
+
+export default Saved;
---
+++ src/components/Wrapper/Wrapper.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import "Wrapper.css";
+
+class Wrapper extends Component {
+
+  render() {
+    return {
+
+    }
+  }
+}
+
+export default Wrapper;
